8688fe5c5e564cc0b8323d418aa38eaf8f3ba098
TrippCreighton <tjc2221@jagmail.southalabama.edu>
2024-09-24 00:19:35 -0500

    removing test code

--- BlueTeamTest/src/Main/Main.java
+++ BlueTeamTest/src/Main/Main.java
@@ -1,23 +1,8 @@
 package Main;
 
-import javax.swing.JFrame;
-
 public class Main {
 	public static void main(String[] args) {
 
-		JFrame window = new JFrame();
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setResizable(false);
-		window.setTitle("Display Test");
-		
-		DisplayPanel displayPanel = new DisplayPanel();
-		window.add(displayPanel);
-		
-		window.pack();
-		
-		window.setLocationRelativeTo(null);
-		window.setVisible(true);
-
-		displayPanel.startDisplayThread();
+		System.out.println("Hello");
 	}
 } 
