be72261a88a7199f7be945e3af1f32914ec32e90
TrippCreighton <tjc2221@jagmail.southalabama.edu>
2024-10-13 13:13:04 -0500

    Added methods for adaptive questioning and more comments

--- MathCoachV1/src/Main/User.java
+++ MathCoachV1/src/Main/User.java
@@ -1,27 +1,87 @@
 package Main;
 
 /**
- * User class will store values related to the user
- * of the math coach such as score and setting values.
+ * User class will store complex meta data about the user
+ * of the math coach program. the primary function of this 
+ * class will be to calculate setting values if the user
+ * selects to have adaptive questioning. This class will also 
+ * store the user's settings and overall score 
  */
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
 
-	private int score;
+	private int averScore;
 	private int avatarVal;
 	private int userSpeed;
 	private int userDifficult;
+	private int adaptSpeed;
+	private int adaptDifficult;
+	private int sessionNum;
+	private boolean isAdaptive;
+	List<MathQuestion> correctQuestion = new ArrayList<>();
+	List<MathQuestion> wrongQuestion = new ArrayList<>();
 	
 	//Constructor
 	public User() { 
-		score = 0;
+		averScore = 0;
 		avatarVal = 0;
 		userSpeed = 0;
+		sessionNum = 0;
 		userDifficult = 0;
+		isAdaptive = false;
 	}
 	
+	
+	public void addCorrectQuestion (MathQuestion question) {
+		correctQuestion.add(question);
+	}
+	
+	public void addWrongQuestion (MathQuestion question) {
+		wrongQuestion.add(question);
+	}
+	
+	
+	//Adaptive methods
+	
+		//THIS METHOD IS UNFINISHED/ THIS IS A SIMPLE VERSION
+		public void calAdaptDifficult() {
+			List<Integer> corrOperand = new ArrayList<>();
+			List<Integer> wrongOperand = new ArrayList<>();
+			for (MathQuestion question : correctQuestion) {
+				corrOperand.add(question.getOperandOne);
+				corrOperand.add(question.getOperandTwo);
+			}
+			for (MathQuestion question : wrongQuestion) {
+				wrongOperand.add(question.getOperandOne);
+				wrongOperand.add(question.getOperandTwo);
+			}
+			if ((corrOperand.size() > wrongOperand.size()) && adaptDifficult < 3) {
+				adaptDifficult ++;
+			}
+			if ((corrOperand.size() < wrongOperand.size()) && adaptDifficult > 0) {
+				adaptDifficult --;
+			}
+		}
+
+		//THIS METHOD IS UNFINISHED/ THIS IS A SIMPLE VERSION
+		public void calAdaptSpeed() {
+			int total = 0;
+			int count = 0;
+			for (MathQuestion question : correctQuestion) {
+				total = total + question.getTimeTaken();
+				count ++;
+			}
+			adaptSpeed = (total / count) - 1;
+		}
+	
 	//Setters
-	public void changeScore(int change) {
-		score = score + change;
+	
+	public void addScore(int change) {
+		averScore = (averScore + change) / sessionNum;
+		sessionNum ++;
 	}
 	
 	public void changeAvatar(int change) {
@@ -36,10 +96,14 @@ public class User {
 		userDifficult = change;
 	}
 	
+	public void setAdaptive(boolean isAdaptive) {
+		this.isAdaptive = isAdaptive;
+	}
 	
 	//Getters
+	
 	public int getScore() {
-		return score;
+		return averScore;
 	}
 	
 	public int getAvatar() {
@@ -53,4 +117,21 @@ public class User {
 	public int getUserDifficult() {
 		return userDifficult;
 	}
+
+	public int getAdaptDifficult() {
+		return adaptDifficult;
+	}
+
+	public int getAdaptSpeed() {
+		return adaptSpeed;
+	}
+
+	public boolean isAdaptive() {
+		return isAdaptive;
+	}
+	
+	
+
+	
+	
 }
